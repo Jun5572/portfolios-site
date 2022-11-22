89c91c01e4ef7dcbe6557e6de3c615c57786215f
= <takizawa.tab@gmail.com>
2022-11-22 22:36:47 +0900

    fix: refs 各種コンポーネント微修正

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,6 @@ import React, { FC } from 'react'
 
 export const Footer: FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 h-24 bg-sky-500">Footer</footer>
+    <footer className="fixed bottom-0 left-0 right-0 h-24 bg-background-secondary">Footer</footer>
   )
 }
--- src/components/GlobalMenu/GlobalMenu.tsx
+++ src/components/GlobalMenu/GlobalMenu.tsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 export const GlobalMenu: FC = () => {
   return (
-    <header className="flex items-center h-16 px-16 bg-sky-500">
+    <header className="flex items-center h-16 px-16 bg-background-primary">
       <nav>
-        <ul className="flex space-x-8 text-white h-full">
-          <li className="text-lg"><NavLink to="/">HOME1</NavLink></li>
-          <li className="text-lg"><NavLink to="/react-router-transition">react-router-transition</NavLink></li>
+        <ul className="inline-flex space-x-6 text-white">
+          <li className="text-lg"><NavLink className="px-3 py-4" to="/">HOME1</NavLink></li>
+          <li className="text-lg"><NavLink className="px-3 py-4" to="/react-router-transition">react-router-transition</NavLink></li>
         </ul>
       </nav>
     </header>
