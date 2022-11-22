8ce92eec24dfb9a204499afd61ca09d2e4e383b4
= <takizawa.tab@gmail.com>
2022-11-22 22:36:06 +0900

    feat: refs tailwindconfigファイル編集

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["public/index.html", "src/**/*.@(ts|tsx)"],
   theme: {
+    // 参照　https://ironodata.info/rgb.php?color=6DCAD4
+    colors: {
+      black: "#333333",
+      white: "#ffffff",
+      reverse: "#6DCAD4",
+      background: {
+        primary: "#6DCAD4",
+        secondary: "#58A5AD",
+        reverse: "#ffffff",
+      },
+      buttonColor: {
+        primary: "#458187",
+      },
+    },
     extend: {},
   },
   plugins: [],
